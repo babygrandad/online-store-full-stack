7d182655432acd6162c09feba904fdb03d4d8dc2
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-04-23 17:34:27 +0200

    add comment to javascript file

--- public/js/customscript.js
+++ public/js/customscript.js
@@ -1,4 +1,7 @@
 const userQuantity = document.getElementById('user-quantity')
+
+// functions that control the incease and decrease of the
+// quantity the user wants to buy
 function stepUp() {
         userQuantity.value = parseInt(userQuantity.value) + 1
 }
