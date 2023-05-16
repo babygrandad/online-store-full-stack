adb390f3ec50007a5ff5749d641e59c548bc1ee1
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-05-16 14:14:33 +0200

    add sandles to DB

--- Planning/SQL databade/online-store-full-stack.sql
+++ Planning/SQL databade/online-store-full-stack.sql
@@ -7,7 +7,7 @@ CREATE TABLE `categories` (
   `category_id` int NOT NULL AUTO_INCREMENT,
   `category_name` varchar(50) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS colors;
 CREATE TABLE `colors` (
@@ -17,9 +17,10 @@ CREATE TABLE `colors` (
   `group_color` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`color_id`),
   UNIQUE KEY `color_hex` (`color_hex`),
+  UNIQUE KEY `color_name` (`color_name`),
   KEY `group_color` (`group_color`),
   CONSTRAINT `colors_ibfk_1` FOREIGN KEY (`group_color`) REFERENCES `group_colors` (`group_color`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS customers;
 CREATE TABLE `customers` (
@@ -78,7 +79,7 @@ CREATE TABLE `products` (
   `price` decimal(10,2) DEFAULT NULL,
   `quantity` int DEFAULT NULL,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS sizes;
 CREATE TABLE `sizes` (
@@ -90,22 +91,22 @@ CREATE TABLE `sizes` (
 
 CREATE OR REPLACE VIEW `all_shoes` AS select `products`.`product_id` AS `product_id`,`products`.`product_name` AS `product_name`,`products`.`product_discription` AS `product_discription`,`products`.`price` AS `price`,`products`.`quantity` AS `quantity`,(select json_arrayagg(`sizes`.`size`) from (`product_sizes` left join `sizes` on((`product_sizes`.`size_id` = `sizes`.`size_id`))) where (`product_sizes`.`product_id` = `products`.`product_id`)) AS `sizes`,(select json_arrayagg(`genders`.`gender`) from (`product_genders` left join `genders` on((`product_genders`.`gender_id` = `genders`.`gender_id`))) where (`product_genders`.`product_id` = `products`.`product_id`)) AS `genders`,(select json_arrayagg(json_object('color_name',`colors`.`color_name`,'color_hex',`colors`.`color_hex`)) from (`product_colors` left join `colors` on((`product_colors`.`color_id` = `colors`.`color_id`))) where (`product_colors`.`product_id` = `products`.`product_id`)) AS `colors`,(select json_arrayagg(`categories`.`category_name`) from (`product_categories` left join `categories` on((`product_categories`.`category_id` = `categories`.`category_id`))) where (`product_categories`.`product_id` = `products`.`product_id`)) AS `categories` from `products` group by `products`.`product_id`,`products`.`product_name`,`products`.`product_discription`,`products`.`price`,`products`.`quantity`;
 
-INSERT INTO categories(category_id,category_name) VALUES(1,'Sneakers'),(2,'Heels'),(3,'Boots'),(4,'Formal'),(5,'Sandles'),(6,'Crocs'),(7,'Trainers');
+INSERT INTO categories(category_id,category_name) VALUES(1,'Sneakers'),(2,'Heels'),(3,'Boots'),(4,'Formal'),(5,'Sandals'),(6,'Crocs'),(7,'Trainers'),(8,'Flip Flop');
 
-INSERT INTO colors(color_id,color_name,color_hex,group_color) VALUES(1,'Black','#000000','Black'),(2,'White','#ffffff','White'),(3,'Grey','#808080','Grey'),(4,'Red','#ff0000','Red'),(5,'Blue','#0000ff','Blue'),(6,'Green','#008000','Green'),(7,'Orange','#ffa500','Orange'),(8,'Pink','#FFC0CB','Pink'),(9,'Silver','#C0C0C0','Silver'),(10,'Purple','#800080','Purple'),(11,'Yellow','#FFFF00','Yellow'),(12,'Brown','#A52A2A','Brown'),(14,'Lime Green','#B7E241','Green'),(15,'Pumpkin','#EA9149','Orange'),(16,'Deep Lavender','#826AD9','Purple'),(17,'Fern Green','#73BF78','Green'),(18,'Sand','#D9AD77','Orange'),(19,'Salmon','#EC5D5D','Red'),(20,'Jade','#338C30','Green'),(21,'Ash','#7C7C7C','Grey'),(22,'Apricot','#E17F4D','Orange'),(23,'Lilac','#A871C5','Purple'),(24,'Ink Blue','#0F2444','Blue'),(25,'Criomson','#BF0404','Red'),(26,'Sky Blue','#04B2D9','Blue');
+INSERT INTO colors(color_id,color_name,color_hex,group_color) VALUES(1,'Black','#000000','Black'),(2,'White','#ffffff','White'),(3,'Grey','#808080','Grey'),(4,'Red','#ff0000','Red'),(5,'Blue','#0000ff','Blue'),(6,'Green','#008000','Green'),(7,'Orange','#ffa500','Orange'),(8,'Pink','#FFC0CB','Pink'),(9,'Silver','#C0C0C0','Silver'),(10,'Purple','#800080','Purple'),(11,'Yellow','#FFFF00','Yellow'),(12,'Brown','#A52A2A','Brown'),(14,'Lime Green','#B7E241','Green'),(15,'Pumpkin','#EA9149','Orange'),(16,'Deep Lavender','#826AD9','Purple'),(17,'Fern Green','#73BF78','Green'),(18,'Sand','#D9AD77','Orange'),(19,'Salmon','#EC5D5D','Red'),(20,'Jade','#338C30','Green'),(21,'Ash','#7C7C7C','Grey'),(22,'Apricot','#E17F4D','Orange'),(23,'Lilac','#A871C5','Purple'),(24,'Ink Blue','#0F2444','Blue'),(25,'Criomson','#BF0404','Red'),(26,'Sky Blue','#04B2D9','Blue'),(27,'Espresso','#40341E','Brown'),(28,'Ever Green','#30592A','Green'),(29,'Maroon','#592E32','Red'),(30,'Baby Blue','#91BDD9','Blue'),(31,'Royal Purple','#6C568C','Purple'),(32,'Seafoam Green','#6DA696','Green'),(33,'Pale Yellow','#E9F2A0','Yellow'),(34,'Periwinkle Purple','#8086C6','Purple');
 
 
 INSERT INTO genders(gender_id,gender) VALUES(1,'Male'),(2,'Female');
 
 INSERT INTO group_colors(group_color,hex_code) VALUES('Black','#000000'),('Blue','#0000FF'),('Brown','#A52A2A'),('Green','#008000'),('Grey','#808080'),('Orange','#FFA500'),('Pink','#FFC0CB'),('Purple','#800080'),('Red','#FF0000'),('Silver','#C0C0C0'),('White','#FFFFFF'),('Yellow','#FFFF00');
 
-INSERT INTO product_categories(product_id,category_id) VALUES(1,1),(1,7),(2,1),(3,1),(3,7),(4,1);
+INSERT INTO product_categories(product_id,category_id) VALUES(1,1),(1,7),(2,1),(3,1),(3,7),(4,1),(5,5),(5,8),(6,5),(6,8),(7,5),(7,6),(8,6),(8,8);
 
-INSERT INTO product_colors(product_id,color_id) VALUES(1,8),(1,14),(1,15),(1,16),(2,8),(2,17),(2,18),(2,19),(3,20),(3,21),(3,22),(3,23),(4,1),(4,21),(4,6),(4,24);
+INSERT INTO product_colors(product_id,color_id) VALUES(1,8),(1,14),(1,15),(1,16),(2,8),(2,17),(2,18),(2,19),(3,20),(3,21),(3,22),(3,23),(4,1),(4,21),(4,6),(4,24),(5,3),(5,25),(5,26),(6,24),(6,27),(6,28),(6,29),(7,30),(7,31),(7,32),(7,29),(7,12),(8,8),(8,26),(8,33),(8,34);
 
-INSERT INTO product_genders(product_id,gender_id) VALUES(1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1);
+INSERT INTO product_genders(product_id,gender_id) VALUES(1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1),(5,1),(6,1),(6,2),(7,1),(7,2),(8,2);
 
-INSERT INTO product_sizes(product_id,size_id) VALUES(1,4),(1,5),(1,6),(1,7),(2,4),(2,5),(2,7),(2,9),(3,5),(3,6),(3,7),(3,8),(4,6),(4,7),(4,8),(4,10);
+INSERT INTO product_sizes(product_id,size_id) VALUES(1,4),(1,5),(1,6),(1,7),(2,4),(2,5),(2,7),(2,9),(3,5),(3,6),(3,7),(3,8),(4,6),(4,7),(4,8),(4,10),(5,6),(5,7),(5,8),(5,9),(6,4),(6,5),(6,7),(6,9),(7,4),(7,5),(7,6),(7,7),(8,4),(8,5),(8,6),(8,7);
 
-INSERT INTO products(product_id,product_name,product_discription,price,quantity) VALUES(1,'Nike SuperRep','Nike SuperRep is a breathable, supportive training shoe with two layers of foam for cushioning and stability, plus a burpee break for mobility.',1300.00,21),(2,'Vans Old Skool','The Vans Old Skool is a classic low-top skate shoe with a canvas and suede upper, iconic side stripe, padded collar, reinforced toe cap, and signature waffle outsole.',700.00,42),(3,'Nike AirStride','Introducing the Nike AirStride: a pinnacle of style and performance. With its cushioned sole, breathable materials, and versatile design, it\'s the ultimate choice for athletic prowess and everyday fashion. Elevate your game.',1000.00,36);
+INSERT INTO products(product_id,product_name,product_discription,price,quantity) VALUES(1,'Nike SuperRep','Nike SuperRep is a breathable, supportive training shoe with two layers of foam for cushioning and stability, plus a burpee break for mobility.',1300.00,21),(2,'Vans Old Skool','The Vans Old Skool is a classic low-top skate shoe with a canvas and suede upper, iconic side stripe, padded collar, reinforced toe cap, and signature waffle outsole.',700.00,42),(3,'Nike AirStride','The Nike AirStride: a pinnacle of style and performance. With its cushioned sole, breathable materials, and versatile design, it\'s the ultimate choice for athletic prowess and everyday fashion. Elevate your game.',1000.00,36),(4,'Aldo SwiftStride','The Aldo SwiftStride: Experience the perfect blend of style and performance. With its sleek design, cushioned comfort, and reliable traction, it\'s the ideal choice for all-day wear. Step up your game with confidence.',800.00,17),(5,'SunBreeze','The SunBreeze Flipflop: Stylish and comfortable, perfect for beach days and summer strolls. Premium materials, cushioned footbed, and durable outsole for relaxed adventures. Casual elegance redefined.',120.00,56),(6,'RVCA Breezy Step','The RVCA Breezy Step: Embrace summer with lightweight comfort. Featuring a soft, breathable footbed and flexible outsole, it\'s your go-to for effortless relaxation and beachside adventures. Experience ultimate freedom for your feet.',120.00,60),(7,'Cloud Esase','The CloudEase Slip-On Sandal: Effortless comfort awaits with our lightweight foam sandal. Slip into cloud-like softness and enjoy a breezy stride for casual outings and relaxing moments. Embrace soothing relaxation for your feet.',250.00,32),(8,'Foam Glide','Experience the next level of comfort with our foam-infused flip-flops. Enjoy plush cushioning, lightweight support, and a flexible fit for ultimate relaxation and carefree adventures. Embrace foam luxury for your feet.',180.00,25);
 INSERT INTO sizes(size_id,size) VALUES(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10);
