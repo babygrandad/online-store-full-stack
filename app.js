5414feba287bc5737060c996e0f0727b1343a6a8
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-07-19 21:27:39 +0200

    client and server validation final implement

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const axios =  require("axios");
+const axios = require("axios");
 const _ = require('lodash');
 const mysql = require('mysql2');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
-const path = require ('path');
-const $ = require ('jquery');
+const path = require('path');
+const $ = require('jquery');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -19,91 +19,73 @@ const saltRounds = 10;
 const app = express();
 
 app.set('view engine', 'ejs');
-// app.use(session({ secret : process.env.SECRET }))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// Configure passport
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-    (email, password, done) => {
-      connection.query(
-        'SELECT * FROM customers WHERE email = ?',
-        [email],
-        (error, results) => {
-          if (error) {
-            console.log(error);
-            return done(error);
-          }
-  
-          if (results.length === 0) {
-            return done(null, false, { message: 'Invalid email or password' });
-          }
-  
-          const user = results[0];
-  
-          // Compare the provided password with the hashed password stored in the database
-          bcrypt.compare(password, user.password, (err, passwordMatch) => {
-            if (err) {
-              console.log(err);
-              return done(err);
-            }
-  
-            if (!passwordMatch) {
-              return done(null, false, { message: 'Invalid email or password' });
-            }
-  
-            // Login successful
-            return done(null, user);
-          });
-        }
-      );
-    }
-  )
-);
-
 //User session tracking initialize.
 app.use(session(
-    {
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: new MySQLStore({
-        // Configure your session store options
-        // For example, provide your MySQL database connection details
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DATABASE
-      }),
-    })
+  {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }),
 );
-
-
-
-
+// Initialize Passport and session support
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-
 // SQL connection funtion
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 });
 // Listen for the Node.js process to exit (related to SQL connection)
 process.on('exit', () => {
-    // Close the MySQL connection
-    connection.end();
+  // Close the MySQL connection
+  connection.end();
 });
+
+
+// Passport configuration
+passport.use(new LocalStrategy({
+  usernameField: 'email', // Specify the field used for the email
+}, (email, password, done) => {
+  // Find the user with the given email in the database
+  connection.query('SELECT * FROM customers WHERE email = ?', [email], (error, results) => {
+    if (error) {
+      return done(error);
+    }
+    if (!results.length) {
+      return done(null, false, { message: 'Invalid email or password' });
+    }
+
+    const user = results[0];
+    // Compare the hashed password
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false, { message: 'Invalid email or password' });
+      }
+      return done(null, user);
+    });
+  });
+}));
+
+// Serialize and Deserialize User (required for session support)
+passport.serializeUser((user, done) => {
+  done(null, user.customer_id);
+});
+
+passport.deserializeUser((id, done) => {
+  connection.query('SELECT * FROM customers WHERE customer_id = ?', [id], (error, results) => {
+    done(error, results[0]);
+  });
+})
 
 
 //Bootstrap pointers for offline use.
@@ -113,82 +95,82 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 
 app.route('/')
-.get((req,res)=>{
+  .get((req, res) => {
 
     const sql = `SELECT * from all_shoes;`;
     connection.query(sql, (error, results, fields) => {
-        if (error) throw error;
-        
-    res.render('home',{pageTitle : "Home",  results : results}); // send the results back to the client
+      if (error) throw error;
+
+      res.render('home', { pageTitle: "Home", results: results }); // send the results back to the client
     });
-});
+  });
 
 app.route('/contact')
-.get((req,res)=>{
-    res.render('contact',{pageTitle : "Contact"})
-});
+  .get((req, res) => {
+    res.render('contact', { pageTitle: "Contact" })
+  });
 
 app.route('/about')
-.get((req,res)=>{
-    res.render('about',{pageTitle : "about"})
-});
+  .get((req, res) => {
+    res.render('about', { pageTitle: "about" })
+  });
 
 
 // Product pages routse
 app.route('/products')
-.get((req,res)=>{
+  .get((req, res) => {
 
     const allShoesSql = `SELECT * FROM all_shoes;`;
     const categorySql = `SELECT category_name FROM categories;`;
     const groupColorSql = `SELECT * FROM group_colors;`;
 
     connection.query(allShoesSql, (error, results, fields) => {
-        if (error) throw error;
+      if (error) throw error;
 
-        connection.query(categorySql, (categoryError, categoryResults, categoryFields) => {
-            if (categoryError) throw categoryError;
+      connection.query(categorySql, (categoryError, categoryResults, categoryFields) => {
+        if (categoryError) throw categoryError;
 
-            connection.query(groupColorSql, (groupColorError, groupColorResults, groupColorFields) => {
-                if (groupColorError) throw groupColorError;
+        connection.query(groupColorSql, (groupColorError, groupColorResults, groupColorFields) => {
+          if (groupColorError) throw groupColorError;
 
-                res.render('products', {
-                    pageTitle: "Shop",
-                    results: results,
-                    categoryResults: categoryResults,
-                    groupColorResults : groupColorResults
-                });
-            });
+          res.render('products', {
+            pageTitle: "Shop",
+            results: results,
+            categoryResults: categoryResults,
+            groupColorResults: groupColorResults
+          });
         });
+      });
     });
-});
+  });
 
 app.route('/products/:shoeID')
-.get((req,res)=>{
+  .get((req, res) => {
 
     let requestedShoe = req.params.shoeID;
 
-    const sql = `SELECT * from all_shoes WHERE product_id =`+ requestedShoe + ` ;`;
+    const sql = `SELECT * from all_shoes WHERE product_id =` + requestedShoe + ` ;`;
     connection.query(sql, (error, results, fields) => {
-        if (error) throw error;
-        
-        res.render('product',{pageTitle : results[0].product_name , shoe : results}); // send the results back to the client
+      if (error) throw error;
+
+      res.render('product', { pageTitle: results[0].product_name, shoe: results }); // send the results back to the client
     });
-});
+  });
 
 app.route('/categories')
-.get((req,res)=>{
-    res.render('categories',{pageTitle : "categories"})
-});
+  .get((req, res) => {
+    res.render('categories', { pageTitle: "categories" })
+  });
 
 
 //sign up routes
 app.route('/signup')
-.get((req,res)=>{
-    res.render('signup',{pageTitle : "signup"})
-})
-.post((req, res) => {
+  .get((req, res) => {
+    res.render('signup', { pageTitle: "signup" })
+  })
+  .post((req, res) => {
     const { fname, lname, email, password, phone } = req.body;
-    
+
     // Hash the password
     bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
       if (err) {
@@ -196,7 +178,7 @@ app.route('/signup')
         res.status(500).send("Error hashing password");
         return;
       }
-      
+
       // Store the user in the database
       connection.query(
         'INSERT INTO customers (customer_first_name, customer_last_name, email, phone, password) VALUES (?,?,?,?,?)',
@@ -207,42 +189,54 @@ app.route('/signup')
             res.status(500).send("Error registering user");
             return;
           }
-          
+
           // Registration successful
           res.status(200).send("User registered successfully");
         }
       );
     });
-});
+  });
 
 
 //Login Routes
 app.route('/login')
-.get((req,res)=>{
-    res.render('login',{pageTitle : "login"})
-})
-.post(
-  passport.authenticate('local', {
-    successRedirect: '/products', // Redirect to the products page upon successful login
-    failureRedirect: '/login', // Redirect back to the login page if authentication fails
+  .get((req, res) => {
+    res.render('login', { pageTitle: "login" })
   })
-);
+  .post((req, res, next) => {
+     // Authenticate using the LocalStrategy we defined earlier
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      // If authentication fails, return the error message
+      console.log(info)
+      return res.status(401).json({ message: info.message });
+      
+    }
 
-
-
-app.route('/test')
-.get((req,res)=>{
-
-    const sql = `SELECT * from all_shoes;`;
-    connection.query(sql, (error, results, fields) => {
-        if (error) throw error;
-        
-    res.send(results); // send the results back to the client
+    // If authentication succeeds, log the user in using req.login()
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      // Return a success message or the user object
+      return res.status(200).json({ message: 'Login successful', user });
     });
-});
+  })(req, res, next);
+  })
+
+  app.route('/test')
+  .get((req, res) => {
+    if (req.isAuthenticated()){
+      res.send('enjoy Luxury');
+    }else{
+      res.redirect('/login');
+    }
+  })
 
 
-
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,30 +1,43 @@
 //client side authentication script for Login page
 $("#loginForm").submit(function(event) {
     event.preventDefault(); // Prevent form submission
-    
-    // Get form fields
-    const emailField =  $('input[name="email"]');
-    const passwordField = $('input[name="password"]');
-    
-    // Get form data values.
-    const formData = $(this).serialize();
-
-    //Send form data to Server and await response
-    axios.post("/login", formData)
-    .then(response => {
-        //Success block, redirect to desired page
-        window.location.href = '/contact';
-    })
-    .catch(error => {
-        //Failure block keep on the login page
-        if (error.response) {
-        $('#validationStatus').removeClass('d-none').text(error.response.data)
-        passwordField.val('')
-        }
-    });
   
-});
+    // Get form fields
+    const emailField = $('input[name="email"]');
+    const passwordField = $('input[name="password"]');
+  
+    // Get form data values.
+    const formData = {
+        email: emailField.val(),
+        password: passwordField.val()
+    }
+    // Send form data to Server and await response
+    axios.post("/login", formData)
+      .then(response => {
+        window.location.href = '/products'
+      })
+      .catch(error => {
+        // Failure block, show error message (status codes 401, 500)
+        if (error.response) {
+          $('#validationStatus').removeClass('d-none').text(error.response.data.message);
+          // Handle the UI to show the error message to the user.
+          passwordField.val('')
+        }
+      });
+  }
+);
+  
 
 function incorectDetails(inputField){
     inputField.addClass('is-invalid')
 }
+
+function checkNoValue(inputField, e) {
+    if (!inputField.val()) {
+        $(inputField).addClass('is-invalid');
+        e.preventDefault();
+    } else {
+        $(inputField).addClass('is-valid').removeClass('is-invalid');
+        return;
+    }
+};
