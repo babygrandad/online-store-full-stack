f22d953a6ca83c4cd4fd065daeb1497548e7fd90
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-08-17 13:58:15 +0200

    new cart logic. needs testing

---
+++ cart planner.js
@@ -0,0 +1,112 @@
+if(isAuthenticated()){
+    //scinario 1. User Makes a Cart as Guest and Then Logs In:
+    if(req.cookies.cart){
+        const cart = req.cookies.cart;
+        const userID = cart.userID;
+
+
+    //check who the cart belongs to.
+        //if cart belongs to guest
+        if (userID.startsWith('Guest :')) {
+            cart.userID = req.user.email
+            cart.updated = new Date().getTime();
+            //rest of code
+
+
+
+            //send response
+            res.cookies('cart', cart, { maxAge: 3600000 })
+                .status(200).send('Cart saved for ' + cart.userID)
+        }
+
+        //if cart belongs to this user 
+        else if (userID === req.user.email){
+            cart.updated = new Date().getTime();
+            //rest of code
+
+
+
+            //send response
+            res.cookies('cart', cart, { maxAge: 3600000 })
+                .status(200).send('Cart saved for ' + cart.userID)
+        }
+
+        //if cart belongs to another user
+        else{
+            let timestamp = new Date().getTime()
+            let cart = {}
+            cart.cartID = uuidv4();
+            cart.userID = req.user.email;
+            cart.created = timestamp;
+            cart.updated = timestamp;
+            //rest of code
+
+
+
+            //send response
+            res.cookies('cart', cart, {maxAge: 3600000})
+                .status(200).send('Cart saved for '+ cart.userID)
+        }
+    }else{
+        // Scinario 2. Logged-In User Creates a Cart:
+        let timestamp = new Date().getTime()
+        let cart = {}
+        cart.cartID = uuidv4();
+        cart.userID = req.user.email;
+        cart.created = timestamp;
+        cart.updated = timestamp;
+        
+        res.cookies('cart', cart, {maxAge: 3600000})
+        .status(200).send('Cart saved for Guest Shopper')
+    }
+}else{
+    if(req.cookies.cart){
+    const cart = req.cookies.cart;
+    const userID = cart.userID;
+    //Check who the cart belongs to
+
+        //if cart belongs belongs to a guest
+        if(userID.startsWith('Guest :')){
+            cart.updated = new Date().getTime();
+            //rest of code
+
+
+
+            //send response
+            res.cookies('cart', cart, { maxAge: 3600000 })
+                .status(200).send('Cart saved for Guest')
+        }
+
+        //if cart belongs to a user
+        else{
+            let timestamp = new Date().getTime()
+            let cart = {}
+            cart.cartID = uuidv4();
+            cart.userID = 'Guest : '+ uuidv4();
+            cart.created = timestamp;
+            cart.updated = timestamp;
+            //rest of code
+
+
+
+            //send response
+            res.cookies('cart', cart, {maxAge: 3600000})
+                .status(200).send('Cart saved for '+ cart.userID)
+        }
+
+    }else{
+        let timestamp = new Date().getTime()
+        let cart = {}
+        cart.cartID = uuidv4();
+        cart.userID = 'Guest : ' + uuidv4();
+        cart.created = timestamp;
+        cart.updated = timestamp;
+        //rest of code
+
+
+
+        //send response
+        res.cookies('cart', cart, { maxAge: 3600000 })
+            .status(200).send('Cart saved for ' + cart.userID)
+    }   
+}
