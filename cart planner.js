a73f0c0e7974a2ff4c5a19a0a2ca3c429d99e825
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-08-21 11:09:07 +0200

    cart needs sql now

--- cart planner.js
+++ cart planner.js
@@ -1,111 +1,4 @@
-if (req.isAuthenticated()) {
-    //scinario 1. User Makes a Cart as Guest and Then Logs In:
-    if (req.cookies.cart) {
-        const cart = req.cookies.cart;
-        const userID = cart.userID;
-
-        //check who the cart belongs to.
-        //if cart belongs to guest
-        if (userID.startsWith('Guest :')) {
-            cart.userID = req.user.email
-            cart.updated = new Date().getTime();
-            //rest of code
-
-            console.log(cart);
-            // send response
-            responseWithCookie(res, cart, cart.userID);
-        }
-
-        //if cart belongs to this user 
-        else if (userID === req.user.email) {
-            cart.updated = new Date().getTime();
-            //rest of code
-
-            console.log(cart);
-            // send response
-            responseWithCookie(res, cart, cart.userID);
-        }
-
-        //if cart belongs to another user
-        else {
-            newCartForAuthUser()
-            //rest of code
-
-            console.log(cart);
-            // send response
-            responseWithCookie(res, cart, cart.userID);
-        }
-    } else {
-        // Scinario 2. Logged-In User Creates a Cart:
-        newCartForAuthUser();
-        // send response
-        responseWithCookie(res, cart, cart.userID);
-    }
-} else {
-    if (req.cookies.cart) {
-        const cart = req.cookies.cart;
-        const userID = cart.userID;
-        //Check who the cart belongs to
-
-        //if cart belongs belongs to a guest
-        if (userID.startsWith('Guest :')) {
-            cart.updated = new Date().getTime();
-            //rest of code
-
-            console.log(cart);
-            // send response
-            responseWithCookie(res, cart, "Guest shopper");
-        }
-
-        //if cart belongs to a user
-        else {
-            newCartForGuest();
-            //rest of code
-
-            console.log(cart);
-            // send response
-            responseWithCookie(res, cart, "Guest shopper");
-        }
-
-    } else {
-        newCartForGuest();
-        //rest of code
-
-        console.log(cart);
-        // send response
-        responseWithCookie(res, cart, "Guest shopper");
-    }
-}
-
-// code is not DRY but gawd it works
-
-function newCartForAuthUser() {
-    let timestamp = new Date().getTime()
-    let cart = {}
-    cart.cartID = uuidv4();
-    cart.userID = req.user.email;
-    cart.created = timestamp;
-    cart.updated = timestamp;
-    return cart;
-}
-
-function newCartForGuest() {
-    let timestamp = new Date().getTime()
-    let cart = {}
-    cart.cartID = uuidv4();
-    cart.userID = 'Guest : ' + uuidv4();
-    cart.created = timestamp;
-    cart.updated = timestamp;
-    return cart;
-}
-
-function responseWithCookie(res, cart, user) {
-    res.cookie('cart', cart, { maxAge: 3600000 })
-        .status(200).send(`Cart saved for ${user}.`)
-}
-
-
-///////////////////////////////////////////////////////////////////////////////////////
+//
 
 function newCartForAuthUser() {
     let timestamp = new Date().getTime()
@@ -211,3 +104,18 @@ function newCartForAuthUser() {
       responseWithCookie(res, cart, "Guest shopper");
     }
   }
+
+/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, sunt.
+Rem ipsum deleniti ex aliquid illo accusantium, voluptatum sed exercitationem
+reiciendis doloribus tempora molestiae suscipit excepturi similique corrupti
+illum saepe minus quibusdam magnam obcaecati laudantium eos mollitia nulla.
+Praesentium iusto iste aspernatur. Et aliquam nam cupiditate voluptatum exercitationem eum,
+possimus modi reiciendis a quia nesciunt, blanditiis aut aliquid veniam odit
+porro. Deserunt illo fugiat quod libero excepturi itaque similique perspiciatis
+repellat nostrum explicabo est facere, veniam distinctio unde. Voluptatibus et suscipit
+asperiores odit amet quam quisquam enim doloremque necessitatibus est ea quae,
+laudantium dolores perferendis quae. Dignissimos consectetur ratione dolores quos
+rem esse temporibus ipsum totam inventore at, 
+nemo excepturi facilis quod sed sit harum sequi reiciendis vel.*/
+
+
