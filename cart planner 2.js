1ddd412c23d5c7e72fc1c9e3898d9f30d40ea8fa
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-09-06 14:55:52 +0200

    LOGGING IN LOGIC - check responses

--- cart planner 2.js
+++ cart planner 2.js
@@ -1,20 +1,20 @@
+//LOGGED IN AND LOGGED OUT LOGIC
 if (req.isAuthenticated()) {
     // user is authenticated and thers a cart on the browser
     if (req.cookies.cart) {
-        
+
     }
     // user is authenticated but there is no cart on the browser
     else {
-        
+
     }
-}
-else {
+} else {
     // user is NOT authenticated and thers a cart on the browser
     if (req.cookies.cart) {
         let cart = req.cookies.cart;
         let userID = cart.userID;
 
-        if(userID.startsWith("Guest :")){
+        if (userID.startsWith("Guest :")) {
             cart.updated = new Date().getTime();
 
             updateGuestCartInDB(req, res, cart, connection);
@@ -23,76 +23,73 @@ else {
     // user is NOT authenticated and there is no cart on the browser
     else {
         let cart = newCartForGuest(); // initialize and populate the cart
-        
+
         newCartToDB(req, res, cart, connection) // add the cart to the database
     }
 }
 
 
-function loginlogic(){
-    
-    if (req.cookies && req.cookies.cart) {// Is there a local cart - Yes
-      let cart = req.cookies.cart
+//LOGGING-IN LOCIC
+if (req.cookies && req.cookies.cart) {// Is there a local cart - Yes
+    let cart = req.cookies.cart
 
-      if(cart.userID.startsWith("Guest :")){ // does the userID start with Guest? - Yes
-        
-            if(checkIfCartExistsInDB(req, res, connection).Length === 1){ //does the user have a cart in the DB? - Yes
+    if (cart.userID.startsWith("Guest :")) { // does the userID start with Guest? - Yes
 
-                const cartResults = checkIfCartExistsInDB(req, res, connection)
+        if (checkIfCartExistsInDB(req, res, connection).Length === 1) { //does the user have a cart in the DB? - Yes
 
-                loginFoundMatchingCart(res, cart, connection, cartResults)
-            }
-            else{ //does the user have a cart in the DB? - No
-                let cart = newCartForAuthUser();
-                newEmptyCartToDB(res, cart, connection);
-            }
+            const cartResults = checkIfCartExistsInDB(req, res, connection)
 
-      }
-      else { // does the userID start with Guest? - No
-
-        let cart = newCartForAuthUser() 
-
-        newEmptyCartToDB(res ,cart ,connection)
-      }
-      
-    }else{// Is there a local cart - no
-      
-        let cart = null;
-
-        if(checkIfCartExistsInDB(req, res, connection).Length === 1){ //does the user have a cart in the DB? - Yes
-
-            const cartResults = checkIfCartExistsInDB(req, res, connection);
-
-            cart.userID = cartResults[0].user_id;
-            cart.cartID = cartResults[0].cart_id;
-            cart.created = cartResults[0].created;
-            cart.updated = cartResults[0].updated;
-
-            res
-                .cookie("cart", cart, { maxAge: 3600000 })
-                .status(200)
-                .send(`Cart saved for ${cart.userID}.`);
+            loginFoundMatchingCart(res, cart, connection, cartResults)
         }
-        else{ //does the user have a cart in the DB? - No
+        else { //does the user have a cart in the DB? - No
             let cart = newCartForAuthUser();
             newEmptyCartToDB(res, cart, connection);
         }
+
+    }
+    else { // does the userID start with Guest? - No
+
+        let cart = newCartForAuthUser()
+
+        newEmptyCartToDB(res, cart, connection)
     }
 
+} else {// Is there a local cart - no
+
+    let cart = null;
+
+    if (checkIfCartExistsInDB(req, res, connection).Length === 1) { //does the user have a cart in the DB? - Yes
+
+        const cartResults = checkIfCartExistsInDB(req, res, connection);
+
+        cart.userID = cartResults[0].user_id;
+        cart.cartID = cartResults[0].cart_id;
+        cart.created = cartResults[0].created;
+        cart.updated = cartResults[0].updated;
+
+        res
+            .cookie("cart", cart, { maxAge: 3600000 })
+            .status(200)
+            .send(`Cart saved for ${cart.userID}.`);
+    }
+    else { //does the user have a cart in the DB? - No
+        let cart = newCartForAuthUser();
+        newEmptyCartToDB(res, cart, connection);
+    }
 }
+
+//LOGGING OUT LOGIC { }
 
 
 // all functions below this point
-
-
 function newCartForAuthUser() {
-let timestamp = new Date().getTime();
-let cart = {};
-cart.cartID = uuidv4();
-cart.userID = req.user.email;
-cart.created = timestamp;
-cart.updated = timestamp;
-return cart;
+    let timestamp = new Date().getTime();
+    let cart = {};
+    cart.cartID = uuidv4();
+    cart.userID = req.user.email;
+    cart.created = timestamp;
+    cart.updated = timestamp;
+    return cart;
 }
 
 function newCartForGuest() {
@@ -177,7 +174,7 @@ async function updateGuestCartInDB(req, res, cart, connection) {
         const insertCartQuery = "UPDATE carts SET modified = ? WHERE cart_id = ?";
         const insertCartValues = [updated, cartID];
         const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
-        
+
 
         // SQL query 2 (using promise-based API)
         const { color, size, quantity, productID, entryID } = req.body;
@@ -223,7 +220,7 @@ async function checkIfCartExistsInDB(req, res, connection) {
 
 async function loginFoundMatchingCart(res, cart, connection, cartResults) {
     const { cart_id, user_id } = cartResults[0];
-    const { cartID} = cart
+    const { cartID } = cart
     const newUpdate = new Date().getTime();
 
     // SQL query 2 (using promise-based API) - update cart_items with the matched cart_id
