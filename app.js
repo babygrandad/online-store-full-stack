24c18682ff9ec01ed8e237583c39cb9cd63dca91
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-09-07 19:56:37 +0200

    focus on user not having DB cart

--- app.js
+++ app.js
@@ -234,6 +234,9 @@ app.route('/cart/add')
           }
         }
 
+        //testing cart results
+        console.log(cart);
+
         // Handle response here
         res
           .cookie("cart", cart, { maxAge: 3600000 })
@@ -261,6 +264,9 @@ app.route('/cart/add')
         const insertItemsValues = [cartID, entryID, productID, color, size, quantity];
         const [itemsResults] = await connection.promise().query(insertItemsQuery, insertItemsValues);
 
+        //testing cart results
+        console.log(cart);
+
         // Handle response here
         res
           .cookie("cart", cart, { maxAge: 3600000 })
@@ -286,6 +292,9 @@ app.route('/cart/add')
         const insertItemsQuery = "INSERT INTO cart_items (cart_id, entry_id, product_id, color, size, quantity) VALUES (?,?,?,?,?,?)";
         const insertItemsValues = [cartID, entryID, productID, color, size, quantity];
         const [itemsResults] = await connection.promise().query(insertItemsQuery, insertItemsValues);
+
+        //testing cart results
+        console.log(cart);
 
         // Handle response here
         res
@@ -365,6 +374,94 @@ app.route('/login')
   })
   .post((req, res, next) => {
 
+    // ! Important ( Moved the functions TO HERE)
+
+    function newCartForAuthUser(user) {
+      let timestamp = new Date().getTime();
+      let cart = {};
+      cart.cartID = uuidv4();
+      cart.userID = user.email;
+      cart.created = timestamp;
+      cart.updated = timestamp;
+      return cart;
+    }
+    async function newEmptyCartToDB(res, cart, connection) {
+      try {
+        const { cartID, userID, created, updated } = cart;
+
+        // SQL query 1 (using promise-based API)
+        const insertCartQuery = "INSERT INTO carts (cart_id, user_id, created, modified) VALUES (?,?,?,?)";
+        const insertCartValues = [cartID, userID, created, updated];
+        const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
+        //testing cart results
+        console.log(cart);
+
+        // Handle response here
+        res
+          .cookie("cart", cart, { maxAge: 3600000 })
+          .status(200)
+          .send(`Login Successful`);
+      } catch (error) {
+        // Handle any error
+        console.error(error);
+        res.status(500).send("Error saving cart. Please try again.");
+      }
+    }
+    async function checkIfCartExistsInDB(res, cart, connection, user) {
+      try {
+        // SQL query 1 (using promise-based API) - check to see if there's a matching
+        // username cart as the logged-in user.
+        const checkCartQuery = "SELECT * FROM carts WHERE user_id = ?";
+        const [cartResults] = await connection.promise().query(checkCartQuery, [user.email]);
+
+        //testing cart results
+        console.log('cart results -: length = ' + cartResults.length);
+
+        if (cartResults.length === 1) { //does the user have a cart in the DB? - Yes
+
+          loginFoundMatchingCart(res, cart, connection, cartResults)
+        }
+        //FOCUS HERE THIS SDJFHA;SLDH;JHG;DJS;AKSJDGAJFSHDJFA;DF;ASLDFHA;LDH;ALDHA;LDH;LKDFHS;LAKSDFH
+        //ADJFHSA;SLKJDHFAKLJDHSF;LAKJSHDKJFLHADSLJFHASLDJFHALDJFHAKSLDJFHAKSLDJFHLAKDJFHSKLASDJFHLAKSJU
+        else { //does the user have a cart in the DB? - No
+          let cart = newCartForAuthUser(user);
+          newEmptyCartToDB(res, cart, connection);
+        }
+      }
+      catch (error) {
+        console.error(error);
+        res.status(500).send("Error finding cart");
+      }
+    }
+    async function loginFoundMatchingCart(res, cart, connection, cartResults) {
+      const { cart_id, user_id } = cartResults[0];
+      const { cartID } = cart
+      const newUpdate = new Date().getTime();
+
+      // SQL query 2 (using promise-based API) - update cart_items with the matched cart_id
+      const updateCartItemsQuery = "UPDATE cart_items SET cart_id = ? WHERE cart_id = ?";
+      const updateCartItemsValues = [cart_id, cartID];
+      const [updateResults] = await connection.promise().query(updateCartItemsQuery, updateCartItemsValues);
+
+      // SQL query 3 (using promise-based API) - update the modified timestamp in carts
+      const updateCartQuery = "UPDATE carts SET modified = ? WHERE user_id = ?";
+      const updateCartValues = [newUpdate, user_id];
+      const [updateCartResults] = await connection.promise().query(updateCartQuery, updateCartValues);
+
+      // Update the cart object with the results and set a cookie
+      cart.userID = user_id;
+      cart.cartID = cart_id;
+      cart.created = cartResults[0].created;
+      cart.updated = newUpdate;
+
+      //testing cart results
+      console.log(cart);
+
+      res
+        .cookie("cart", cart, { maxAge: 3600000 })
+        .status(200)
+        .send(`Login Successful`);
+    }
 
     // Authenticate using the LocalStrategy we defined earlier
     passport.authenticate('local', (err, user, info) => {
@@ -382,133 +479,29 @@ app.route('/login')
           return next(err);
         }
 
-        if (req.cookies && req.cookies.cart) {// Is there a local cart - Yes
+        if (req.cookies.cart) {// Is there a local cart - Yes
           let cart = req.cookies.cart
 
           if (cart.userID.startsWith("Guest :")) { // does the userID start with Guest? - Yes
 
-            const cartResults = checkIfCartExistsInDB(req, res, connection);
-
-            if (cartResults.Length === 1) { //does the user have a cart in the DB? - Yes
-
-              loginFoundMatchingCart(res, cart, connection, cartResults)
-            }
-            else { //does the user have a cart in the DB? - No
-              let cart = newCartForAuthUser();
-              newEmptyCartToDB(res, cart, connection);
-            }
-
+            // check to see if theres a db cart (condition check is in this function)
+            checkIfCartExistsInDB(res, cart, connection, user);
           }
           else { // does the userID start with Guest? - No
 
-            let cart = newCartForAuthUser()
-
+            let cart = newCartForAuthUser(user)
             newEmptyCartToDB(res, cart, connection)
           }
 
         } else {// Is there a local cart - no
 
-          let cart = null;
+          let cart = newCartForAuthUser(user);
 
-          const cartResults = checkIfCartExistsInDB(res, connection);
-
-          if (cartResults.Length === 1) { //does the user have a cart in the DB? - Yes
-
-            cart.userID = cartResults[0].user_id;
-            cart.cartID = cartResults[0].cart_id;
-            cart.created = cartResults[0].created;
-            cart.updated = cartResults[0].updated;
-
-            //testing cart results
-            console.log(cart);
-
-            res
-              .cookie("cart", cart, { maxAge: 3600000 })
-              .status(200)
-              .send(`Login Successful`);
-          }
-          else { //does the user have a cart in the DB? - No
-            let cart = newCartForAuthUser();
-            newEmptyCartToDB(res, cart, connection);
-          }
+          // check to see if theres a db cart (condition check is in this function)
+          checkIfCartExistsInDB(res, cart, connection, user);
         }
 
-        function newCartForAuthUser() {
-          let timestamp = new Date().getTime();
-          let cart = {};
-          cart.cartID = uuidv4();
-          cart.userID = req.user.email;
-          cart.created = timestamp;
-          cart.updated = timestamp;
-          return cart;
-        }
-        async function newEmptyCartToDB(res, cart, connection) {
-          try {
-            const { cartID, userID, created, updated } = cart;
-
-            // SQL query 1 (using promise-based API)
-            const insertCartQuery = "INSERT INTO carts (cart_id, user_id, created, modified) VALUES (?,?,?,?)";
-            const insertCartValues = [cartID, userID, created, updated];
-            const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
-
-            //testing cart results
-            console.log(cart);
-
-            // Handle response here
-            res
-              .cookie("cart", cart, { maxAge: 3600000 })
-              .status(200)
-              .send(`Login Successful`);
-          } catch (error) {
-            // Handle any error
-            console.error(error);
-            res.status(500).send("Error saving cart. Please try again.");
-          }
-        }
-        async function checkIfCartExistsInDB(res, connection) {
-          try {
-            // SQL query 1 (using promise-based API) - check to see if there's a matching
-            // username cart as the logged-in user.
-            const checkCartQuery = "SELECT * FROM carts WHERE user_id = ?";
-            const [cartResults] = await connection.promise().query(checkCartQuery, [user.email]);
-
-            return cartResults;
-
-          }
-          catch (error) {
-            console.error(error);
-            res.status(500).send("Error finding cart");
-          }
-        }
-        async function loginFoundMatchingCart(res, cart, connection, cartResults) {
-          const { cart_id, user_id } = cartResults[0];
-          const { cartID } = cart
-          const newUpdate = new Date().getTime();
-
-          // SQL query 2 (using promise-based API) - update cart_items with the matched cart_id
-          const updateCartItemsQuery = "UPDATE cart_items SET cart_id = ? WHERE cart_id = ?";
-          const updateCartItemsValues = [cart_id, cartID];
-          const [updateResults] = await connection.promise().query(updateCartItemsQuery, updateCartItemsValues);
-
-          // SQL query 3 (using promise-based API) - update the modified timestamp in carts
-          const updateCartQuery = "UPDATE carts SET modified = ? WHERE user_id = ?";
-          const updateCartValues = [newUpdate, user_id];
-          const [updateCartResults] = await connection.promise().query(updateCartQuery, updateCartValues);
-
-          // Update the cart object with the results and set a cookie
-          cart.userID = user_id;
-          cart.cartID = cart_id;
-          cart.created = cartResults[0].created;
-          cart.updated = newUpdate;
-
-          //testing cart results
-          console.log(cart);
-
-          res
-            .cookie("cart", cart, { maxAge: 3600000 })
-            .status(200)
-            .send(`Login Successful`);
-        }
+        // ! Important ( Moved the functions from here)
       });
     })(req, res, next);
   });
--- cart planner 2.js
+++ cart planner 2.js
@@ -1,40 +1,124 @@
 //LOGGING-IN LOGIC
-if (req.cookies && req.cookies.cart) {// Is there a local cart - Yes
-    let cart = req.cookies.cart
+function cartLogicFirst() {
+    if (req.cookies.cart) {// Is there a local cart - Yes
+        let cart = req.cookies.cart
 
-    if (cart.userID.startsWith("Guest :")) { // does the userID start with Guest? - Yes
+        if (cart.userID.startsWith("Guest :")) { // does the userID start with Guest? - Yes
 
-        const cartResults = checkIfCartExistsInDB(req, res, connection);
+            const cartResults = checkIfCartExistsInDB(res, connection, user);
+            console.log("What if statement sees if thers is a match: ", cartResults)
+            if (cartResults.length === 1) { //does the user have a cart in the DB? - Yes
 
-        if (cartResults.Length === 1) { //does the user have a cart in the DB? - Yes
+                loginFoundMatchingCart(res, cart, connection, cartResults)
+            }
+            else { //does the user have a cart in the DB? - No
+                let cart = newCartForAuthUser();
+                newEmptyCartToDB(res, cart, connection);
+            }
 
-            loginFoundMatchingCart(res, cart, connection, cartResults)
+        }
+        else { // does the userID start with Guest? - No
+
+            let cart = newCartForAuthUser()
+
+            newEmptyCartToDB(res, cart, connection)
+        }
+
+    } else {// Is there a local cart - no
+
+        let cart = null;
+
+        const cartResults = checkIfCartExistsInDB(res, connection, user);
+        console.log("What if statement sees if thers no match: ", cartResults)
+        if (cartResults.length === 1) { //does the user have a cart in the DB? - Yes
+
+            cart.userID = cartResults[0].user_id;
+            cart.cartID = cartResults[0].cart_id;
+            cart.created = cartResults[0].created;
+            cart.updated = cartResults[0].updated;
+
+            //testing cart results
+            console.log(cart);
+
+            res
+                .cookie("cart", cart, { maxAge: 3600000 })
+                .status(200)
+                .send(`Login Successful`);
         }
         else { //does the user have a cart in the DB? - No
             let cart = newCartForAuthUser();
             newEmptyCartToDB(res, cart, connection);
         }
-
-    }
-    else { // does the userID start with Guest? - No
-
-        let cart = newCartForAuthUser()
-
-        newEmptyCartToDB(res, cart, connection)
     }
 
-} else {// Is there a local cart - no
+    function newCartForAuthUser() {
+        let timestamp = new Date().getTime();
+        let cart = {};
+        cart.cartID = uuidv4();
+        cart.userID = req.user.email;
+        cart.created = timestamp;
+        cart.updated = timestamp;
+        return cart;
+    }
+    async function newEmptyCartToDB(res, cart, connection) {
+        try {
+            const { cartID, userID, created, updated } = cart;
 
-    let cart = null;
+            // SQL query 1 (using promise-based API)
+            const insertCartQuery = "INSERT INTO carts (cart_id, user_id, created, modified) VALUES (?,?,?,?)";
+            const insertCartValues = [cartID, userID, created, updated];
+            const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
+            //testing cart results
+            console.log(cart);
 
-    const cartResults = checkIfCartExistsInDB(res, connection);
+            // Handle response here
+            res
+                .cookie("cart", cart, { maxAge: 3600000 })
+                .status(200)
+                .send(`Login Successful`);
+        } catch (error) {
+            // Handle any error
+            console.error(error);
+            res.status(500).send("Error saving cart. Please try again.");
+        }
+    }
+    async function checkIfCartExistsInDB(res, connection, user) {
+        try {
+            // SQL query 1 (using promise-based API) - check to see if there's a matching
+            // username cart as the logged-in user.
+            const checkCartQuery = "SELECT * FROM carts WHERE user_id = ?";
+            const [cartResults] = await connection.promise().query(checkCartQuery, [user.email]);
 
-    if (cartResults.Length === 1) { //does the user have a cart in the DB? - Yes
+            //testing cart results
+            console.log('cart results -: ' + [cartResults]);
+            return cartResults;
 
-        cart.userID = cartResults[0].user_id;
-        cart.cartID = cartResults[0].cart_id;
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).send("Error finding cart");
+        }
+    }
+    async function loginFoundMatchingCart(res, cart, connection, cartResults) {
+        const { cart_id, user_id } = cartResults[0];
+        const { cartID } = cart
+        const newUpdate = new Date().getTime();
+
+        // SQL query 2 (using promise-based API) - update cart_items with the matched cart_id
+        const updateCartItemsQuery = "UPDATE cart_items SET cart_id = ? WHERE cart_id = ?";
+        const updateCartItemsValues = [cart_id, cartID];
+        const [updateResults] = await connection.promise().query(updateCartItemsQuery, updateCartItemsValues);
+
+        // SQL query 3 (using promise-based API) - update the modified timestamp in carts
+        const updateCartQuery = "UPDATE carts SET modified = ? WHERE user_id = ?";
+        const updateCartValues = [newUpdate, user_id];
+        const [updateCartResults] = await connection.promise().query(updateCartQuery, updateCartValues);
+
+        // Update the cart object with the results and set a cookie
+        cart.userID = user_id;
+        cart.cartID = cart_id;
         cart.created = cartResults[0].created;
-        cart.updated = cartResults[0].updated;
+        cart.updated = newUpdate;
 
         //testing cart results
         console.log(cart);
@@ -44,92 +128,118 @@ if (req.cookies && req.cookies.cart) {// Is there a local cart - Yes
             .status(200)
             .send(`Login Successful`);
     }
-    else { //does the user have a cart in the DB? - No
-        let cart = newCartForAuthUser();
-        newEmptyCartToDB(res, cart, connection);
+}
+
+
+//LOGGING-IN LOGIC RE-REVISED
+function cartLogicSecond() {
+    if (req.cookies.cart) {// Is there a local cart - Yes
+        let cart = req.cookies.cart
+
+        if (cart.userID.startsWith("Guest :")) { // does the userID start with Guest? - Yes
+
+            // check to see if theres a db cart (condition check is in this function)
+            checkIfCartExistsInDB(res, cart, connection, user);
+        }
+        else { // does the userID start with Guest? - No
+
+            let cart = newCartForAuthUser(user)
+            newEmptyCartToDB(res, cart, connection)
+        }
+
+    } else {// Is there a local cart - no
+
+        let cart = newCartForAuthUser(user);
+
+       // check to see if theres a db cart (condition check is in this function)
+       checkIfCartExistsInDB(res, cart, connection, user);
     }
-}
 
-function newCartForAuthUser() {
-    let timestamp = new Date().getTime();
-    let cart = {};
-    cart.cartID = uuidv4();
-    cart.userID = req.user.email;
-    cart.created = timestamp;
-    cart.updated = timestamp;
-    return cart;
-}
-async function newEmptyCartToDB(res, cart, connection) {
-    try {
-        const { cartID, userID, created, updated } = cart;
+    function newCartForAuthUser(user) {
+        let timestamp = new Date().getTime();
+        let cart = {};
+        cart.cartID = uuidv4();
+        cart.userID = user.email;
+        cart.created = timestamp;
+        cart.updated = timestamp;
+        return cart;
+    }
+    async function newEmptyCartToDB(res, cart, connection) {
+        try {
+            const { cartID, userID, created, updated } = cart;
 
-        // SQL query 1 (using promise-based API)
-        const insertCartQuery = "INSERT INTO carts (cart_id, user_id, created, modified) VALUES (?,?,?,?)";
-        const insertCartValues = [cartID, userID, created, updated];
-        const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
+            // SQL query 1 (using promise-based API)
+            const insertCartQuery = "INSERT INTO carts (cart_id, user_id, created, modified) VALUES (?,?,?,?)";
+            const insertCartValues = [cartID, userID, created, updated];
+            const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
+            //testing cart results
+            console.log(cart);
+
+            // Handle response here
+            res
+                .cookie("cart", cart, { maxAge: 3600000 })
+                .status(200)
+                .send(`Login Successful`);
+        } catch (error) {
+            // Handle any error
+            console.error(error);
+            res.status(500).send("Error saving cart. Please try again.");
+        }
+    }
+    async function checkIfCartExistsInDB(res, cart, connection, user) {
+        try {
+            // SQL query 1 (using promise-based API) - check to see if there's a matching
+            // username cart as the logged-in user.
+            const checkCartQuery = "SELECT * FROM carts WHERE user_id = ?";
+            const [cartResults] = await connection.promise().query(checkCartQuery, [user.email]);
+
+            //testing cart results
+            console.log('cart results -: ' + [cartResults]);
+
+            if (cartResults.length === 1) { //does the user have a cart in the DB? - Yes
+
+                loginFoundMatchingCart(res, cart, connection, cartResults)
+            }
+            else { //does the user have a cart in the DB? - No
+                let cart = newCartForAuthUser(user);
+                newEmptyCartToDB(res, cart, connection);
+            }
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).send("Error finding cart");
+        }
+    }
+    async function loginFoundMatchingCart(res, cart, connection, cartResults) {
+        const { cart_id, user_id } = cartResults[0];
+        const { cartID } = cart
+        const newUpdate = new Date().getTime();
+
+        // SQL query 2 (using promise-based API) - update cart_items with the matched cart_id
+        const updateCartItemsQuery = "UPDATE cart_items SET cart_id = ? WHERE cart_id = ?";
+        const updateCartItemsValues = [cart_id, cartID];
+        const [updateResults] = await connection.promise().query(updateCartItemsQuery, updateCartItemsValues);
+
+        // SQL query 3 (using promise-based API) - update the modified timestamp in carts
+        const updateCartQuery = "UPDATE carts SET modified = ? WHERE user_id = ?";
+        const updateCartValues = [newUpdate, user_id];
+        const [updateCartResults] = await connection.promise().query(updateCartQuery, updateCartValues);
+
+        // Update the cart object with the results and set a cookie
+        cart.userID = user_id;
+        cart.cartID = cart_id;
+        cart.created = cartResults[0].created;
+        cart.updated = newUpdate;
 
         //testing cart results
         console.log(cart);
 
-        // Handle response here
         res
             .cookie("cart", cart, { maxAge: 3600000 })
             .status(200)
             .send(`Login Successful`);
-    } catch (error) {
-        // Handle any error
-        console.error(error);
-        res.status(500).send("Error saving cart. Please try again.");
     }
 }
-async function checkIfCartExistsInDB(res, connection) {
-    try {
-        // SQL query 1 (using promise-based API) - check to see if there's a matching
-        // username cart as the logged-in user.
-        const checkCartQuery = "SELECT * FROM carts WHERE user_id = ?";
-        const [cartResults] = await connection.promise().query(checkCartQuery, [user.email]);
-
-        return cartResults;
-
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).send("Error finding cart");
-    }
-}
-async function loginFoundMatchingCart(res, cart, connection, cartResults) {
-    const { cart_id, user_id } = cartResults[0];
-    const { cartID } = cart
-    const newUpdate = new Date().getTime();
-
-    // SQL query 2 (using promise-based API) - update cart_items with the matched cart_id
-    const updateCartItemsQuery = "UPDATE cart_items SET cart_id = ? WHERE cart_id = ?";
-    const updateCartItemsValues = [cart_id, cartID];
-    const [updateResults] = await connection.promise().query(updateCartItemsQuery, updateCartItemsValues);
-
-    // SQL query 3 (using promise-based API) - update the modified timestamp in carts
-    const updateCartQuery = "UPDATE carts SET modified = ? WHERE user_id = ?";
-    const updateCartValues = [newUpdate, user_id];
-    const [updateCartResults] = await connection.promise().query(updateCartQuery, updateCartValues);
-
-    // Update the cart object with the results and set a cookie
-    cart.userID = user_id;
-    cart.cartID = cart_id;
-    cart.created = cartResults[0].created;
-    cart.updated = newUpdate;
-
-    //testing cart results
-    console.log(cart);
-
-    res
-        .cookie("cart", cart, { maxAge: 3600000 })
-        .status(200)
-        .send(`Login Successful`);
-}
-
-
-
-
 
 
 
@@ -167,8 +277,8 @@ if (req.isAuthenticated()) { //Is the user logged in? - Yes
         }
     } else { // is there a cart in the browser? - No
 
-        let cart = newCartForGuest(); 
-        newCartToDB(req, res, cart, connection) 
+        let cart = newCartForGuest();
+        newCartToDB(req, res, cart, connection)
     }
 }
 
@@ -198,11 +308,14 @@ async function newCartToDB(req, res, cart, connection) {
 
         function determineUser() {
             if (userID.startsWith("Guest :")) {
-              return "Guest User";
+                return "Guest User";
             } else {
-              return userID;
+                return userID;
             }
         }
+
+        //testing cart results
+        console.log(cart);
 
         // Handle response here
         res
@@ -231,6 +344,9 @@ async function updateGuestCartInDB(req, res, cart, connection) {
         const insertItemsValues = [cartID, entryID, productID, color, size, quantity];
         const [itemsResults] = await connection.promise().query(insertItemsQuery, insertItemsValues);
 
+        //testing cart results
+        console.log(cart);
+
         // Handle response here
         res
             .cookie("cart", cart, { maxAge: 3600000 })
@@ -244,27 +360,30 @@ async function updateGuestCartInDB(req, res, cart, connection) {
 }
 async function updateAuthCartInDB(req, res, cart, connection) {
     try {
-      const { userID, cartID, updated } = cart;
+        const { userID, cartID, updated } = cart;
 
-      // SQL query 1 (using promise-based API)
-      const insertCartQuery = "UPDATE carts SET modified = ? WHERE cart_id = ?";
-      const insertCartValues = [updated, cartID];
-      const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
+        // SQL query 1 (using promise-based API)
+        const insertCartQuery = "UPDATE carts SET modified = ? WHERE cart_id = ?";
+        const insertCartValues = [updated, cartID];
+        const [cartResults] = await connection.promise().query(insertCartQuery, insertCartValues);
 
-      // SQL query 2 (using promise-based API)
-      const { color, size, quantity, productID, entryID } = req.body;
-      const insertItemsQuery = "INSERT INTO cart_items (cart_id, entry_id, product_id, color, size, quantity) VALUES (?,?,?,?,?,?)";
-      const insertItemsValues = [cartID, entryID, productID, color, size, quantity];
-      const [itemsResults] = await connection.promise().query(insertItemsQuery, insertItemsValues);
+        // SQL query 2 (using promise-based API)
+        const { color, size, quantity, productID, entryID } = req.body;
+        const insertItemsQuery = "INSERT INTO cart_items (cart_id, entry_id, product_id, color, size, quantity) VALUES (?,?,?,?,?,?)";
+        const insertItemsValues = [cartID, entryID, productID, color, size, quantity];
+        const [itemsResults] = await connection.promise().query(insertItemsQuery, insertItemsValues);
 
-      // Handle response here
-      res
-        .cookie("cart", cart, { maxAge: 3600000 })
-        .status(200)
-        .send(`Cart saved for ${userID}.`);
+        //testing cart results
+        console.log(cart);
+
+        // Handle response here
+        res
+            .cookie("cart", cart, { maxAge: 3600000 })
+            .status(200)
+            .send(`Cart saved for ${userID}.`);
     } catch (error) {
-      // Handle any error
-      console.error(error);
-      res.status(500).send("Error saving cart. Please try again");
+        // Handle any error
+        console.error(error);
+        res.status(500).send("Error saving cart. Please try again");
     }
 }
