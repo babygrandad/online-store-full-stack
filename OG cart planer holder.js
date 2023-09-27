493c27567d67fc2fbd490fa12dddaa23e41ac8e0
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-09-27 14:55:25 +0200

    updated data attributes for items

--- OG cart planer holder.js
+++ OG cart planer holder.js
@@ -1,77 +1,23 @@
-app.route('/cart/update')
-  .patch((req, res) => {
-    const { newQuantity, productId, entryId } = req.body;
-    const cart = req.cookies.cart;
+// Function to filter products by color
+function filterProductsByColor(color) {
+  const productElements = document.querySelectorAll('.col-sm-6.col-lg-4.py-3');
 
-    // Start a transaction to ensure all queries succeed or fail together
-    connection.beginTransaction(function (beginTransactionErr) {
-      if (beginTransactionErr) {
-        console.error(beginTransactionErr);
-        return res.status(500).json({ error: 'Database error' });
-      }
+  productElements.forEach((productElement) => {
+    const productColor = productElement.getAttribute('data-color');
+    const colors = JSON.parse(productColor);
 
-      // Update the quantity in the 'cart_items' table
-      const updateQuantitySql = 'UPDATE cart_items SET quantity = ? WHERE product_id = ? AND entry_id = ?';
-      const updateQuantityValues = [newQuantity, productId, entryId];
-
-      connection.query(updateQuantitySql, updateQuantityValues, function (updateQuantityErr, updateQuantityResults) {
-        if (updateQuantityErr) {
-          // Rollback the transaction on error
-          connection.rollback(function () {
-            console.error(updateQuantityErr);
-            return res.status(500).json({ error: 'Database error' });
-          });
-        } else {
-          // Update the 'modified' timestamp in the 'carts' table
-          const updateModifiedSql = 'UPDATE carts SET modified = ? WHERE cart_id = ?';
-          const currentTime = new Date().getTime();
-          const updateModifiedValues = [currentTime, cart.cartID];
-
-          connection.query(updateModifiedSql, updateModifiedValues, function (updateModifiedErr, updateModifiedResults) {
-            if (updateModifiedErr) {
-              // Rollback the transaction on error
-              connection.rollback(function () {
-                console.error(updateModifiedErr);
-                return res.status(500).json({ error: 'Database error' });
-              });
-            } else {
-              // Execute the SELECT query to calculate the total quantity in 'cart_items'
-              const calculateTotalQuantitySql = 'SELECT SUM(quantity) AS count_value FROM cart_items WHERE cart_id = ?';
-              const calculateTotalQuantityValues = [cart.cartID];
-
-              connection.query(calculateTotalQuantitySql, calculateTotalQuantityValues, function (calculateTotalQuantityErr, calculateTotalQuantityResults) {
-                if (calculateTotalQuantityErr) {
-                  // Rollback the transaction on error
-                  connection.rollback(function () {
-                    console.error(calculateTotalQuantityErr);
-                    return res.status(500).json({ error: 'Database error' });
-                  });
-                } else {
-                  // Commit the transaction if all queries are successful
-                  connection.commit(function (commitErr) {
-                    if (commitErr) {
-                      // Rollback the transaction on error
-                      connection.rollback(function () {
-                        console.error(commitErr);
-                        return res.status(500).json({ error: 'Database error' });
-                      });
-                    } else {
-                      // Send a success response with the updated total quantity
-                      const totalQuantity = calculateTotalQuantityResults[0].count_value;
-                      cart.updated = currentTime;
-                      cart.sumQuantity = totalQuantity;
-                      console.log(cart);
-                      return res
-                        .cookie("cart", cart, { expires: new Date(new Date().getTime() + 3600000) })
-                        .status(200)
-                        .send('Item removed from cart and modified timestamp updated successfully');
-                    }
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-    });
+    // Check if the selected color is in the product's colors array
+    if (colors.some((c) => c.color_name === color)) {
+      productElement.style.display = 'block'; // Show the product
+    } else {
+      productElement.style.display = 'none'; // Hide the product
+    }
   });
+}
+
+// Example usage:
+filterProductsByColor('Pink'); // Filter products by color 'Pink'
+
+
+
+
