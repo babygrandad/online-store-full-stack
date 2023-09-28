973ac4ffcf95010b11f0b7c34494372c87c027f7
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-09-28 12:20:50 +0200

    gender filter Complete

--- public/js/filterSortSearch.js
+++ public/js/filterSortSearch.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    let counter = 1;
 
 
 
@@ -25,3 +24,20 @@ function filterProductsByColor(color) {
     });
 }
 
+// Function to filter products by gender
+function filterProductsByGender(gender) {
+  $('.shoe-component').each(function () {
+    const genders = $(this).data('gender');
+
+    // Check if the selected gender is in the product's genders array
+    if ((genders.includes('Male') && genders.includes('Female'))) {
+      $(this).show();
+    } else if (genders.includes(gender)) {
+      $(this).show(); // Show the product
+    }else if(gender === 'All'){
+      $('.shoe-component').show(); // Show all products
+    } else {
+      $(this).hide(); // Hide the product
+    }
+  });
+}
