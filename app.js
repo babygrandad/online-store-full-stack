937a3b653554023a6137c28d1ac40ff9804876d0
babygrandad <80920573+babygrandad@users.noreply.github.com>
2023-10-13 13:48:16 +0200

    use env.PORT for render

--- app.js
+++ app.js
@@ -884,6 +884,6 @@ app.route('/test')
    res.send()
   });
 
-app.listen(3000, function () {
+app.listen( process.env.PORT || 3000, function () {
   console.log("Server started on port 3000 on " + new Date());
 });
